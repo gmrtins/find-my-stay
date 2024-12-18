71191257e79d211a61196d2c256b970a44103332
Gonçalo Martins <g.m.g@live.com.pt>
2024-12-18 19:58:06 +0000

    add hotels list
    Added Axios.
    Created ListItem component.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,13 +6,22 @@ export default function TabLayout() {
     const { t } = useTranslation();
 
     return (
-        <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+        <Tabs screenOptions={{ tabBarActiveTintColor: "#1A94FF" }}>
             <Tabs.Screen
                 name="index"
                 options={{
                     title: t("tab_homepage"),
                     tabBarIcon: ({ color }) => (
-                        <FontAwesome size={28} name="home" color={color} />
+                        <FontAwesome size={20} name="home" color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="Favorites"
+                options={{
+                    title: t("tab_favorites"),
+                    tabBarIcon: ({ color }) => (
+                        <FontAwesome size={20} name="bookmark-o" color={color} />
                     ),
                 }}
             />
@@ -21,7 +30,7 @@ export default function TabLayout() {
                 options={{
                     title: t("tab_settings"),
                     tabBarIcon: ({ color }) => (
-                        <FontAwesome size={28} name="gear" color={color} />
+                        <FontAwesome size={20} name="gear" color={color} />
                     ),
                 }}
             />
