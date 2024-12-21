6871a6c5365225e3dc98d742df6db860d6696887
Gonçalo Martins <g.m.g@live.com.pt>
2024-12-21 17:34:15 +0000

    add new components to hotel details view

--- app/screens/HotelDetails.tsx
+++ app/screens/HotelDetails.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity, Linking } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity } from 'react-native';
 import { useLocalSearchParams } from 'expo-router'
 import Carousel from 'react-native-reanimated-carousel';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
@@ -6,15 +6,29 @@ import Stars from '../components/Stars';
 import { useTranslation } from 'react-i18next';
 import { IHotel } from '../types';
 import { getRatingMessage } from '../utils';
+import React, { useContext, useMemo, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import CheckInOutTimes from '../components/CheckInOutTimes';
+import { FavoritesContext } from '../contexts/FavoriteContext';
+import colors from '../theme/colors';
 
 export default function HotelDetails() {
+    const [isLoading, setIsLoading] = useState(true);
+    const { t } = useTranslation();
+    const { favorites, add, remove } = useContext(FavoritesContext);
     const params = useLocalSearchParams();
     const hotel: IHotel = JSON.parse(Array.isArray(params.data) ? params.data[0] : params.data);
     const width = Dimensions.get('window').width;
-    const { t } = useTranslation();
+    const PlaceholderImageUri = 'https://blocks.astratic.com/img/general-img-portrait.png'; // Replace this with your placeholder image URL
+
+    const isFavorite = useMemo(() => favorites.includes(hotel.id), [favorites, hotel.id]);
+
     return (
         <View style={styles.container}>
             <View style={{ flex: 1, maxHeight: 200 }}>
+                <TouchableOpacity style={styles.favoriteBtn} onPress={() => isFavorite ? remove(hotel.id) : add(hotel.id)}>
+                    <FontAwesome name={isFavorite ? "bookmark" : "bookmark-o"} size={20} color={colors.BLUE} />
+                </TouchableOpacity>
                 <Carousel
                     loop
                     width={width}
@@ -22,12 +36,15 @@ export default function HotelDetails() {
                     autoPlay={true}
                     data={hotel.gallery}
                     scrollAnimationDuration={1000}
-                    renderItem={({ item }) => (
-                        <View style={{ backgroundColor: "blue" }}>
-                            <Image source={{ uri: item as string }} style={{ width: width, height: width / 2 }} resizeMode='cover' />
-                        </View>
-                    )}
-                />
+                    renderItem={({ item }) => {
+                        const [imageUri, setImageUri] = useState(item);
+                        return (
+                            <View>
+                                <Image source={{ uri: imageUri as string }} onError={() => setImageUri(PlaceholderImageUri)} style={{ width: width, height: width / 2 }} resizeMode='cover' />
+                            </View>);
+                    }}
+                    style={{ zIndex: -1 }} />
+
             </View>
             <View style={styles.infoContainer}>
                 <Text style={styles.title}>{hotel.name}</Text>
@@ -35,14 +52,9 @@ export default function HotelDetails() {
                     <Text>{t("hotel_details_hotel_label")}</Text>
                     <Stars rating={hotel.stars} />
                 </View>
-                <View style={styles.addressContainer}>
-                    <FontAwesome name="map-marker" size={25} color="#1A94FF" />
-                    <Text>{hotel.location.address + ' - ' + hotel.location.city}</Text>
-                </View>
                 <View style={styles.hoursContainer}>
-                    <Text>{hotel.checkIn.from + ' ' + hotel.checkIn.to}</Text>
-                    <Text>{hotel.checkOut.from + ' ' + hotel.checkOut.to}</Text>
-
+                    <CheckInOutTimes title={t('hotel_details_checkin_label')} time={hotel.checkIn} />
+                    <CheckInOutTimes title={t('hotel_details_checkout_label')} time={hotel.checkOut} />
                 </View>
 
                 <View style={styles.userRatingContainer}>
@@ -50,7 +62,7 @@ export default function HotelDetails() {
                     <View style={styles.ratingContainer}>
 
                         <View style={styles.ratingMessageContainer}>
-                            <View style={[styles.contactBtn, { width: 20, height: 20 }]} >
+                            <View style={[styles.contactBtn, { width: 20, height: 20 }]}>
                                 <FontAwesome name="thumbs-up" size={10} color="white" />
                             </View>
                             <Text style={styles.ratingMessageLabel1}>{getRatingMessage(hotel.userRating)}</Text>
@@ -61,19 +73,41 @@ export default function HotelDetails() {
                     </View>
 
                 </View>
-                {hotel.contact.phoneNumber &&
+
+
+
+                <View style={styles.addressContainer}>
+                    <FontAwesome name="map-marker" size={25} color={colors.BLUE} />
+                    <Text>{hotel.location.address + ' - ' + hotel.location.city}</Text>
+                </View>
+
+
+                {/* {hotel.contact.phoneNumber &&
                     <TouchableOpacity style={styles.contactBtn} onPress={() => Linking.openURL(`tel:${hotel.contact.phoneNumber}`)}>
                         <FontAwesome name="phone" size={20} color="white" />
-                    </TouchableOpacity>
-                }
+                    </TouchableOpacity>}
                 {hotel.contact.email &&
                     <TouchableOpacity style={styles.contactBtn} onPress={() => Linking.openURL(`mailto:${hotel.contact.email}`)}>
                         <FontAwesome name="envelope" size={20} color="white" />
-                    </TouchableOpacity>
-                }
+                    </TouchableOpacity>} */}
 
+                <MapView style={styles.map}
+                    initialRegion={{
+                        latitude: hotel.location.latitude,
+                        longitude: hotel.location.longitude,
+                        latitudeDelta: 0.002,
+                        longitudeDelta: 0.002,
+                    }}
+                    onMapReady={() => { setIsLoading(false); }}
+
+
+                >
+                    <Marker coordinate={{ latitude: hotel.location.latitude, longitude: hotel.location.longitude }} />
+                </MapView>
             </View>
-        </View>
+
+
+        </View >
     );
 }
 
@@ -97,13 +131,15 @@ const styles = StyleSheet.create({
     },
     hotelStarsContainer: { flexDirection: 'row', alignItems: 'center', alignSelf: 'flex-start', backgroundColor: '#F0F8FF', paddingHorizontal: 8, paddingVertical: 4, gap: 5, borderRadius: 16 },
     addressContainer: { flexDirection: 'row', alignItems: 'center', gap: 8 },
-    contactBtn: { backgroundColor: '#1A94FF', borderRadius: 100, width: 35, height: 35, alignItems: 'center', justifyContent: 'center' },
-    hoursContainer: {},
-    userRatingLabel: { fontSize: 32, fontWeight: 'bold', color: '#1A94FF' },
-    ratingMessageLabel1: { fontSize: 14, fontWeight: 'bold', color: '#1A94FF' },
+    contactBtn: { backgroundColor: colors.BLUE, borderRadius: 100, width: 35, height: 35, alignItems: 'center', justifyContent: 'center' },
+    hoursContainer: { flexDirection: 'row', justifyContent: 'space-between', gap: 8 },
+    userRatingLabel: { fontSize: 32, fontWeight: 'bold', color: colors.BLUE },
+    ratingMessageLabel1: { fontSize: 14, fontWeight: 'bold', color: colors.BLUE },
     ratingMessageLabel2: { fontSize: 12, color: '#808089' },
     userRatingContainer: { flexDirection: 'row', gap: 8, alignItems: 'center' },
     ratingContainer: { flexDirection: 'column' },
-    ratingMessageContainer: { flexDirection: 'row', alignItems: 'center', gap: 8 }
+    ratingMessageContainer: { flexDirection: 'row', alignItems: 'center', gap: 8 },
+    map: { width: '100%', height: 200, borderRadius: 16 },
+    favoriteBtn: { position: 'absolute', right: 16, top: 16, backgroundColor: 'rgba(0,0,0,0.5)', padding: 8, borderRadius: 100 },
 
 });
--- app/types/index.ts
+++ app/types/index.ts
@@ -28,3 +28,8 @@ export interface IHotel {
     price: number;
     currency: string;
 }
+
+export type HotelDetailsParams = {
+    name: string;
+    data: string;
+};
